41bbe06e016bef09c71ca4da37852c1addf99ad9
arocasan <mr@michelantonio.com>
2023-05-06 23:09:01 +0200

    GARO-1 #comment TODAY FOUNDgit commit -m GARO-1

--- src/spot_price.py
+++ src/spot_price.py
@@ -10,23 +10,34 @@ month = "05"
 day = "06"
 price_area = "SE3"
 url = f"{url}{year}/{month}-{day}_{price_area}"
+now = datetime.datetime.now()
 
 
 def get_spot_price():
-    price_today = requests.get(f"{url}.json")
+    
+    s = requests.Session()
+    price_today = s.get(f"{url}.json")
     price_today = price_today.json()
 
     for hour in price_today:
+        
         time_start = hour["time_start"]
         time_end = hour["time_end"]
         sek_per_kwh = hour["SEK_per_kWh"] * 100
 
-        print(time_start,sek_per_kwh)
+
+        print(time_start[11:16],time_end[11:16], "-", sek_per_kwh)
+
+        if str(now.hour) in time_start[11:13]:
+            print("dagens pris", sek_per_kwh,time_start)
 
     
     
 
 
+get_spot_price()
 
+now = datetime.datetime.now()
+print(now.hour)
 print(it_today)
 #today = get_spot_price()
